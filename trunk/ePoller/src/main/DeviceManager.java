dd21b9dfaf48dc85b6a760d714e4b419707ac6ad
julian.orlando.pena@gmail.com <julian.orlando.pena@gmail.com@a3cedc8e-bbdd-e7fc-fa4b-c1f829ae4c64>
2014-03-19 20:39:16 +0000

    Some improvements over the configuration files handling. Basic trap receiver completed. Now there is a configuration file for the traps you're interested in catching and a traps log file where the info is logged.

--- trunk/ePoller/src/main/DeviceManager.java
+++ trunk/ePoller/src/main/DeviceManager.java
@@ -2,31 +2,45 @@ package main;
 
 import java.io.IOException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import pojos.Parameter;
+import java.util.concurrent.ConcurrentHashMap;
 
 import util.CSVImporter;
 import util.TextWriter;
 
 public final class DeviceManager {
 
-	public static HashMap<String, String> devices;
-	public static ArrayList<Parameter> parameters;
+	public static ConcurrentHashMap<String, String> devices;
+	public static ConcurrentHashMap<String, String> parameters;
+	public static ConcurrentHashMap<String, String> traps;
 
 	public static void loadDevices() throws IOException {
 		devices = CSVImporter.loadDevices("devices.csv");
 	}
-
+	
 	public static void loadParameters() throws IOException {
 		parameters = CSVImporter.loadParameters("parameters.csv");
 	}
-
+	
+	public static void loadTraps() throws IOException {
+		traps = CSVImporter.loadTraps("traps.csv");
+	}
+	
+	public static String getDeviceName(String deviceIP){
+		return devices.get(deviceIP);
+	}
+	
+	public static String getParameterName(String OID){
+		return parameters.get(OID);
+	}
+	
+	public static String getTrapName(String OID){
+		return traps.get(OID);
+	}
+	
 	public static synchronized void writeData(String deviceIP, String data, long latency) {
 		TextWriter.printlnToFile(devices.get(deviceIP), getCurrentDate() + "," + data + latency);
 	}
-
+	
 	public static String getCurrentDate() {
 		return new Timestamp(System.currentTimeMillis()).toString();
 	}
